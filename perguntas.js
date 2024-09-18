e5c1cda37022d205b48932b10c7b2f4e9e8722f9
anapaulasilvastocberl <160019789+anapaulasilvastocberl@users.noreply.github.com>
2024-09-18 19:43:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte? ',
+    'um líder militar e político francês que se tornou imperador da França'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A capital do Brasil é BrasÍlia'
+)
+
+criaCartao(
+    'Charada',
+    ' O que o filho do matemático fala quando quer ir ao banheiro ?',
+    'Pi-Pi '
+)
+
+criaCartao(
+    'charada',
+    'O que é o que é? feito para andar e não anda ',
+    'A rua'
+)
+
+criaCartao(
+    'Geografia',
+    'qual país tem mais ilhas no mundo?',
+    'Suécia, por volta de 220.000'
+)
+criaCartao(
+    'geografia ',
+    'Qual a montanha mai alta do mundo ?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'geografia',
+    'quais os menores países do mundo?',
+    'Vaticano e Rússia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz peixe em Inglês?',
+    'peixe em ingles é fish '
+)
+
+criaCartao(
+    'matemática?',
+    'quanto é 10 mais 10 ?',
+    'é 20'
+)
